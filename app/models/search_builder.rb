a55991b3f4b537220367d99e6552412c74e8cbdd
Darren Hardy <drh@stanford.edu>
2017-05-25 15:36:23 -0700

    update to ArcLight 5/25 3pm

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
+  include Arclight::SearchBehavior
   include BlacklightRangeLimit::RangeLimitBuilder
 
 
