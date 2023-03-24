9d9f737f95b7ee040869107e04488cc6a5dcd18b
Cory Lown <corylown@stanford.edu>
2023-03-24 15:56:23 -0400

    Use oembed_controller instead of oembed_viewer

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,5 +12,5 @@ pin "blacklight/blacklight", to: "blacklight/blacklight.js"
 pin "dialog-polyfill", to: "https://ga.jspm.io/npm:dialog-polyfill@0.5.6/dist/dialog-polyfill.js"
 pin "arclight", to: "arclight/arclight.js"
 # TODO: We may be able to move these to a single importmap for arclight.
-pin "arclight/oembed_viewer", to: "arclight/oembed_viewer.js"
+pin "arclight/oembed_controller", to: "arclight/oembed_controller.js"
 pin "arclight/truncate_controller", to: "arclight/truncate_controller.js"
