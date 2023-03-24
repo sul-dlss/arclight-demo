37eb9f7dc88124a14993a1f7d14bbd8d2a196966
Cory Lown <corylown@stanford.edu>
2023-03-24 15:59:08 -0400

    Do not pin blacklight/blacklight.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@github/auto-complete-element", to: "https://cdn.skypack.dev/@github/auto-complete-element"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/dist/umd/popper.min.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.2/dist/js/bootstrap.js"
-pin "blacklight/blacklight", to: "blacklight/blacklight.js"
 pin "dialog-polyfill", to: "https://ga.jspm.io/npm:dialog-polyfill@0.5.6/dist/dialog-polyfill.js"
 pin "arclight", to: "arclight/arclight.js"
 # TODO: We may be able to move these to a single importmap for arclight.
