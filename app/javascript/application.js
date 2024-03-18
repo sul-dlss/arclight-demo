3e2e9b00de984befd138a9b59877cf0b65b7bf54
Cory Lown <corylown@stanford.edu>
2024-03-18 11:13:35 -0400

    WIP

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-import "arclight"
 import bootstrap from "bootstrap"
 import githubAutoCompleteElement from "@github/auto-complete-element"
 import Blacklight from "blacklight"
+import "arclight"
